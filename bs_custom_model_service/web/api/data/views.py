7881bc9d136ee400ed6181dd2c1d049c47a52151
sanmathibs <sanmathi@blueskycreations.com.au>
2023-04-21 19:05:13 +1000

    update view

--- bs_custom_model_service/web/api/data/views.py
+++ bs_custom_model_service/web/api/data/views.py
@@ -2,13 +2,15 @@ import io
 import tempfile
 import uuid
 from pathlib import Path
-
+from fastapi import FastAPI, File
 import numpy as np
 import pandas as pd
 from fastapi import APIRouter, UploadFile
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import StreamingResponse
 from starlette.responses import JSONResponse
+import os
+import subprocess
 
 from bs_custom_model_service.web.api.models.request.csv_conversion import (
     CSVConversionRequest,
@@ -46,6 +48,7 @@ async def execute_python_script(file: UploadFile):
     :param file: Uploaded py script.
     :return: Result of execution.
     """
+
     contents = await file.read()
     with tempfile.TemporaryDirectory() as tempdir:
         script_file = Path(tempdir) / f"{uuid.uuid4()}.py"
@@ -56,3 +59,38 @@ async def execute_python_script(file: UploadFile):
         except ExecutionFailedError as e:
             response = {"output": str(e), "is_exception_raised": True}
     return JSONResponse(content=jsonable_encoder(response))
+
+@router.post("/script and data file/")
+async def process_data(script: UploadFile, data: UploadFile):
+
+    # Save the data file to a temporary directory
+    data_contents = await data.read()
+    with tempfile.TemporaryDirectory() as tempdir:
+        data_file = Path(tempdir) / f"{uuid.uuid4()}.json"
+        data_file.write_bytes(data_contents)
+
+    # Save the script file to a temporary directory
+    script_contents = await script.read()
+    with tempfile.TemporaryDirectory() as tempdir:
+        script_file = Path(tempdir) / f"{uuid.uuid4()}.py"
+        script_file.write_bytes(script_contents)
+
+
+    # Run the script file on the data file
+    # cmd = f"python {script_file} {data_file}"
+    # output = subprocess.check_output(cmd, shell=True, text=True)
+    #
+    # return {"output": output}
+        try:
+            output = run_command(["python", script_file.as_posix()])
+            response = {"output": output, "is_exception_raised": False}
+        except ExecutionFailedError as e:
+            response = {"output": str(e), "is_exception_raised": True}
+    return JSONResponse(content=jsonable_encoder(response))
+
+@router.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    return {"filename": file.filename, "contents": contents}
+
+
